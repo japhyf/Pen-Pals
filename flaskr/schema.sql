36388139f4968219fa94c421f0333140fdea4358
Abteen Ebrahimi <abteen.ebrahimi@gmail.com>
2018-10-14 16:28:52 -0700

    todo

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -4,6 +4,11 @@ DROP TABLE IF EXISTS user;
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
-
+  password TEXT NOT NULL,
+  first TEXT,
+  last TEXT,
+  street TEXT,
+  city TEXT,
+  state TEXT,
+  zip INTEGER
 );
