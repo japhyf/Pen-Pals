991fd41e2ab5885f5887fc5821741230e9376f55
Abteen Ebrahimi <abteen.ebrahimi@gmail.com>
2018-10-14 16:10:55 -0700

    database working

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -4,12 +4,6 @@ DROP TABLE IF EXISTS user;
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL,
-  email TEXT NOT NULL,
-  first TEXT NOT NULL,
-  last TEXT NOT NULL,
-  street TEXT NOT NULL,
-  state TEXT NOT NULL,
-  zip TEXT NOT NULL
+  password TEXT NOT NULL
 
 );
