f782664ca07cc7a72097244b06f3c76d295fca38
Abteen Ebrahimi <abteen.ebrahimi@gmail.com>
2018-10-14 14:31:49 -0700

    simpler base

--- base.py
+++ base.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
-app = Flask(__name__,)
+from flask import Flask
+app = Flask(__name__)
 
-@app.route('/<string:page_name>/')
-def render_static(page_name):
-    return render_template('%s.html' % page_name)
+@app.route('/')
+def hello():
+    return 'Hello world!'
+
+
+if __name__ == '__main__':
+    app.run()
