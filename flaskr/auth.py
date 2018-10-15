bd67d6c12f223165141a0b71727791b37ec85148
Japhy Frolick <japhyf@gmail.com>
2018-10-14 20:26:36 -0700

    finished getting register/login working with our html

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -13,29 +13,46 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        regUsername = request.form['regUsername']
+        regPassword = request.form['regPassword']
+        loginUsername = request.form['loginUsername']
+        loginPassword = request.form['loginPassword']
         db = get_db()
         error = None
+        if not loginUsername and not loginPassword:
+            if not regUsername:
+                return redirect(url_for('auth.noRegUser'))
+            elif not regPassword:
+                return redirect(url_for('auth.noRegPass'))
+            elif db.execute(
+                'SELECT id FROM user WHERE username = ?', (regUsername,)
+            ).fetchone() is not None:
+                error = 'User {} is already registered.'.format(regUsername)
+            
+            if error is None:
+                db.execute(
+                    'INSERT INTO user (username, password) VALUES (?, ?)',
+                    (regUsername, generate_password_hash(regPassword))
+                )
+                db.commit()
+                return redirect(url_for('auth.login'))
+            flash(error)
+        else:
+            user = db.execute(
+                'SELECT * FROM user WHERE username = ?', (loginUsername,)
+            ).fetchone()
 
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
-            error = 'User {} is already registered.'.format(username)
+            if user is None:
+                error = 'Incorrect username.'
+            elif not check_password_hash(user['password'], loginPassword):
+                error = 'Incorrect password.'
 
-        if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
-            return redirect(url_for('auth.login'))
-
-        flash(error)
+            if error is None:
+                session.clear()
+                session['user_id'] = user['id']
+                return redirect(url_for('index'))
+ 
+            flash(error)
 
     return render_template('auth/register.html')
 
@@ -43,8 +60,8 @@ def register():
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form['loginUsername']
+        password = request.form['loginPassword']
         db = get_db()
         error = None
         user = db.execute(
