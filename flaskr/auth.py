33c0e2241a1c4aced69def26a1afa295f03eb5c8
root <root@DESKTOP-5JOJBFT.localdomain>
2018-10-16 12:33:23 -0700

    added edit_bio and bio pages for editing and viewing bio

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -120,9 +120,9 @@ def create_bio():
         'username': username   
     }
     return render_template('main/create_bio.html', user=user_details)
-    
-@bp.route('/home')
-def home():
+   
+@bp.route('/bio')
+def bio():
     user_id = session.get('user_id')
     db = get_db()
     if user_id is None:
@@ -130,11 +130,101 @@ def home():
     user = db.execute(
         'SELECT * FROM user WHERE id = ?', (user_id,)
     ).fetchone()
+    if user['first'] is None:
+        first = ""
+    else:
+        first = user['first']
+    if user['email'] is None:
+        email = ""
+    else:
+        email = user['email']
+    if user['last'] is None:
+        last = ""
+    else:
+        last = user['last']
+    if user['address_line1'] is None:
+        address_line1 = ""
+    else:
+        address_line1 = user['address_line1']
+    if user['address_line2'] is None:
+        address_line2 = ""
+    else:
+        address_line2 = user['address_line2']
+    if user['username'] is None:
+        username = ""
+    else:
+        username = user['username']
     user_details = {      
-		'email': user['email'],
+        'first': first,
+        'last': last,
+        'email': email,
+        'address1': address_line1,
+        'address2': address_line2,
+        'username': username   
     }
-    return render_template('main/home.html', user=user_details)
+    return render_template('main/bio.html', user=user_details)
+   
+@bp.route('/edit_bio')
+def edit_bio():
+    user_id = session.get('user_id')
+    db = get_db()
+    if user_id is None:
+        return redirect(url_for('auth.start_page'))
+    user = db.execute(
+        'SELECT * FROM user WHERE id = ?', (user_id,)
+    ).fetchone()
+    if user['first'] is None:
+        first = ""
+    else:
+        first = user['first']
+    if user['email'] is None:
+        email = ""
+    else:
+        email = user['email']
+    if user['last'] is None:
+        last = ""
+    else:
+        last = user['last']
+    if user['address_line1'] is None:
+        address_line1 = ""
+    else:
+        address_line1 = user['address_line1']
+    if user['address_line2'] is None:
+        address_line2 = ""
+    else:
+        address_line2 = user['address_line2']
+    if user['username'] is None:
+        username = ""
+    else:
+        username = user['username']
+    user_details = {      
+        'first': first,
+        'last': last,
+        'email': email,
+        'address1': address_line1,
+        'address2': address_line2,
+        'username': username   
+    }
+    return render_template('main/edit_bio.html', user=user_details)
     
+@bp.route('/edit_bio', methods=('GET', 'POST'))
+def edit_bio_form():
+    user_id = session.get('user_id')
+    if request.method == 'POST':
+        db = get_db()
+        error = None
+        #if the register button is clicked load the register inputs
+        first = request.form['first']
+        last = request.form['last']
+        email = request.form['email']
+        username = request.form['username']
+        address1 = request.form['address1']
+        address2 = request.form['address2']
+        sql = 'UPDATE user SET first = ?, last = ?, email = ?, username = ?, address_line1 = ?, address_line2 = ? WHERE id = ?'
+        val = (first, last, email, username, address1, address2, user_id)
+        db.execute(sql, val)
+        db.commit()
+        return redirect(url_for('auth.db'))
     
 @bp.route('/create_bio', methods=('GET', 'POST'))
 def create_bio_form():
@@ -155,6 +245,7 @@ def create_bio_form():
         db.commit()
         return redirect(url_for('auth.db'))
        
+
 @bp.route('/db')
 def db():
     user_id = session.get('user_id')
@@ -181,6 +272,19 @@ def update_email():
         db.commit()
         return redirect(url_for('auth.db'))
 
+@bp.route('/home')
+def home():
+    user_id = session.get('user_id')
+    db = get_db()
+    if user_id is None:
+        return redirect(url_for('auth.start_page'))
+    user = db.execute(
+        'SELECT * FROM user WHERE id = ?', (user_id,)
+    ).fetchone()
+    user_details = {      
+		'email': user['email'],
+    }
+    return render_template('main/home.html', user=user_details)
     
     
 @bp.route('/start_page')
