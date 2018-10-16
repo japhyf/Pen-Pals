e3a747a9c019267c77ad83717c294c7a51cc247b
root <root@DESKTOP-5JOJBFT.localdomain>
2018-10-16 11:19:51 -0700

    everything to do with logging in and registering working

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -38,7 +38,7 @@ def register():
                 ).fetchone()
                 session.clear()
                 session['user_id'] = user['id']
-                return redirect(url_for('auth.index'))
+                return redirect(url_for('auth.create_bio'))
             flash(error)
         #else if the login button is clicked load the login inputs
         else:
@@ -56,7 +56,7 @@ def register():
             if error is None:
                 session.clear()
                 session['user_id'] = user['id']
-                return redirect(url_for('auth.index'))
+                return redirect(url_for('auth.home'))
  
             flash(error)
 
@@ -78,8 +78,51 @@ def logout():
     return redirect(url_for('auth.start_page'))
     
 	
-@bp.route('/index')
-def index():
+@bp.route('/create_bio')
+def create_bio():
+    user_id = session.get('user_id')
+    db = get_db()
+    if user_id is None:
+        return redirect(url_for('auth.start_page'))
+    user = db.execute(
+        'SELECT * FROM user WHERE id = ?', (user_id,)
+    ).fetchone()
+    if user['first'] is None:
+        first = ""
+    else:
+        first = user['first']
+    if user['email'] is None:
+        email = ""
+    else:
+        email = user['email']
+    if user['last'] is None:
+        last = ""
+    else:
+        last = user['last']
+    if user['address_line1'] is None:
+        address_line1 = ""
+    else:
+        address_line1 = user['address_line1']
+    if user['address_line2'] is None:
+        address_line2 = ""
+    else:
+        address_line2 = user['address_line2']
+    if user['username'] is None:
+        username = ""
+    else:
+        username = user['username']
+    user_details = {      
+        'first': first,
+        'last': last,
+        'email': email,
+        'address1': address_line1,
+        'address2': address_line2,
+        'username': username   
+    }
+    return render_template('main/create_bio.html', user=user_details)
+    
+@bp.route('/home')
+def home():
     user_id = session.get('user_id')
     db = get_db()
     if user_id is None:
@@ -90,10 +133,11 @@ def index():
     user_details = {      
 		'email': user['email'],
     }
-    return render_template('main/index.html', user=user_details)
+    return render_template('main/home.html', user=user_details)
     
-@bp.route('/index', methods=('GET', 'POST'))
-def create_bio():
+    
+@bp.route('/create_bio', methods=('GET', 'POST'))
+def create_bio_form():
     user_id = session.get('user_id')
     if request.method == 'POST':
         db = get_db()
@@ -113,6 +157,10 @@ def create_bio():
        
 @bp.route('/db')
 def db():
+    user_id = session.get('user_id')
+    db = get_db()
+    if user_id is None:
+        return redirect(url_for('auth.start_page'))
     db = get_db()
     data = db.execute(
         'SELECT * FROM user'
@@ -139,7 +187,7 @@ def update_email():
 def start_page():
     user_id = session.get('user_id')
     if user_id is not None:
-        return redirect(url_for('main.index'))
+        return redirect(url_for('main.create_bio'))
     else:
         return render_template('auth/start_page.html')
 
