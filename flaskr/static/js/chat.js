ff1ab28ba0b1f5bf1e8528f08770af8a3e95037c
nicholas-a-stull <nickfrog11@gmail.com>
2018-11-04 13:13:03 -0800

    chat ui + current users info

--- flaskr/static/js/chat.js
+++ flaskr/static/js/chat.js
@@ -1,8 +1,46 @@
-var sb = new SendBird({appId: APP_ID});
+function changetext()
+{
+  document.getElementById("heading").innerHTML = response;
+}
+/*
+function ajaxer()
+{
+  console.log('im being called');
+  $.ajax({
+         url: '/main/chat',
+         data: ,
+         type: 'POST',
+         success: function(response) {
+           document.getElementById("heading").innerHTML = response;
+         },
+         error: function(error) {
+             console.log(error);
+         }
+     });
+}
 
-sb.connect(USER_ID, function(user, error) {if (error){return;}});
+
+var sb = new SendBird({appId: "A698EF3E-71F1-4E3A-B762-58A53E356846"});
+
+sb.connect("testbot1", "ff6c17787c277e2deaa98797a240487199510a1a", function(user, error)
+  {
+    if (error)
+    {
+      return;
+    }
+  }
+);
 
 sb.OpenChannel.createChannel(function(openChannel, error)
+  {
+    if (error)
+      {
+        return;
+      }
+  }
+);
+
+sb.OpenChannel.getChannel("frogsfrogsfrogs", function(openChannel, error)
   {
     if (error)
     {
@@ -16,19 +54,13 @@ sb.OpenChannel.createChannel(function(openChannel, error)
           return;
         }
       })
-  }
-);
 
-opeChannel.sendUserMessage(MESSAGE, DATA, CUSTOM_TYPE, function(message, error)
-  {
-    if (error)
-    {
-      return;
+    openChannel.sendUserMessage("hello!", DATA, USER_MESSAGE, function(message, error)
+      {
+        if (error)
+        {
+          return;
+        }
+      })
     }
-  });
-
-
-function changetext()
-{
-  document.getElementById("heading").innerHTML = "idid";
-}
+);*/
