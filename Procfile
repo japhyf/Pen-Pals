321b45d6ee4eb0e3cf654cd15133675644fea3cd
japhyf <japhyf@gmail.com>
2019-01-16 13:06:52 -0800

    more heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn flaskr:app
+web: flask db upgrade; flask translate compile; gunicorn penpals:app
--- penpals.py
+++ penpals.py
@@ -1,11 +1,4 @@
-from app import create_app, db, cli
-from app.models import User, Post, Message, Notification, Task
+from flaskr import create_app, db, cli
 
 app = create_app()
 cli.register(app)
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
-            'Notification': Notification, 'Task': Task}
